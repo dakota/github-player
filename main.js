307fa9ee7f5223eb40d7960728adcef52221e03b
Walther Lalk <waltherlalk@gmail.com>
2017-03-08 14:40:21 +0200

    Add slack command

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var githubhook = require('githubhook');
 var fs = require('fs');
 var player = require('play-sound')(opts = {});
 var slug = require('slug');
 var config = require('config');
 var Util = require('util');
 
-var github = githubhook(config.get('GitHub'));
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var ghHandler = createHandler({ path: config.get('ghPath'), secret: config.get('secret') });
+var bl = require('bl');
+var querystring = require('querystring');
 
 function playRandomSound(sourceFile)
 {
@@ -18,7 +21,7 @@ function playRandomSound(sourceFile)
   fs.readFile(fileName, 'utf8', function (err,data) {
     var lines = data.split('\n');
     var soundFile = lines[Math.floor(Math.random()*(lines.length - 1))];
-  
+
     console.log(Util.format('Playing %s from the %s list', soundFile, sourceFile));
     player.play('sounds/' + soundFile);
   });
@@ -35,7 +38,48 @@ function initialise()
   }
 }
 
-github.on('issues', function (repo, ref, data) {
+var httpServer = http.createServer(function (req, res) {
+  function hasError (msg) {
+    res.writeHead(400)
+    res.end(msg)
+  }
+
+  if (req.url.split('?').shift() === config.get('speakPath') && req.method === 'POST') {
+    req.pipe(bl(function (err, data) {
+      try {
+        obj = querystring.parse(data.toString())
+      } catch (e) {
+        return hasError(e.msg)
+      }
+
+      if (obj.token !== config.get('token')) {
+        return hasError('Invalid token');
+      }
+
+      var request = {
+        ie: 'UTF-8',
+        client: 'tw-ob',
+        q: obj.text,
+        tl: 'En-gb'
+      };
+      player.play('http://translate.google.com/translate_tts?' + querystring.stringify(request), {player: 'mplayer', mplayer: ['-af', 'volume=' + config.get('volume')]});
+
+      res.statusCode = 200;
+      res.end('Your words will be spoken.');
+    }));
+
+    return;
+  }
+  
+  ghHandler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  });
+});
+
+ghHandler.on('issues', function (event) {
+  var data = event.payload;
+
   if (!data.action || data.action !== 'labeled') {
     return;
   }
@@ -45,7 +89,9 @@ github.on('issues', function (repo, ref, data) {
   playRandomSound('label/' + fileName);
 });
 
-github.on('pull_request', function (repo, ref, data) {
+ghHandler.on('pull_request', function (event) {
+  var data = event.payload;
+
   if (!data.action || data.action !== 'closed') {
     return;
   }
@@ -61,4 +107,6 @@ github.on('pull_request', function (repo, ref, data) {
 });
 
 initialise();
-github.listen();
+httpServer.listen(config.get('port'), config.get('host'), function() {
+  console.log('Listening now');
+});
